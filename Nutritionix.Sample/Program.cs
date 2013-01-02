e5ab3a7fe501f2225bc9fdb9ba6e1f515ad513a5
carolynvs <me@carolynvanslyck.com>
2013-01-01 23:02:41 -0600

    Added samples

---
+++ Nutritionix.Sample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Nutritionix.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Samples.Nutritionix.NutritionixSamples.RunAll();
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Nutritionix.Sample/Samples/Nutritionix/NutritionixSamples.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using Nutritionix;
+
+namespace Samples.Nutritionix
+{
+    public static class NutritionixSamples
+    {
+		// TODO: Update with your Nutritionix API ID and Key
+        private const string myApiId = "123";
+        private const string myApiKey = "abc456";
+
+        public static void RunAll()
+        {
+            Search();
+			Retrieve();
+        }
+
+        public static void Search()
+        {
+            var nutritionix = new NutritionixClient();
+            nutritionix.Initialize(myApiId, myApiKey);
+
+            var request = new NutritionixSearchRequest { Query = "pie" };
+			Console.WriteLine("Searching Nutritionix for 'pie'...");
+            NutritionixSearchResponse response = nutritionix.Search(request);
+
+			Console.WriteLine("Displaying results 1 - {0} of {1}", response.TotalResultsReturned, response.TotalResultsMatching);
+            foreach(NutritionixSearchResult result in response.Results)
+            {
+                Console.WriteLine("* {0}", result.ItemName);
+            }
+
+			Console.WriteLine();
+        }
+
+        public static void Retrieve()
+        {
+            var nutritionix = new NutritionixClient();
+            nutritionix.Initialize(myApiId, myApiKey);
+
+            Console.WriteLine("Retrieving 'Raspberry Pie' from Nutritionix...");
+            NutritionixItem item = nutritionix.Retrieve("C9eyVTt8wiSH7EhOPUBl");
+
+            Console.WriteLine("Item Id: {0}", item.ItemId);
+            Console.WriteLine("Item Name: {0}", item.ItemName);
+            Console.WriteLine("Brand Name: {0}", item.BrandName);
+
+            var calories = item.NutritionFacts.First(x => x.Name == NutritionFactType.Calories);
+            Console.WriteLine("Calories: {0} {1}", calories.Value, calories.Unit);
+
+            var sugar = item.NutritionFacts.First(x => x.Name == NutritionFactType.Sugars);
+            Console.WriteLine("Sugar: {0} {1}", sugar.Value, sugar.Unit);
+
+            Console.WriteLine();
+        }
+    }
+}
